cd93e1943c07e7f20e33fd9d30da1165203db86a
Josh Lindsay <git@j000sh.com>
2021-04-19 17:12:30 -0700

    add python virtualenv vars to fish

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -5,4 +5,4 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 alias code codium
 alias xcode 'open -a Xcode'
 
-source (brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+set -g VIRTUALENVWRAPPER_PYTHON (command which python3)
