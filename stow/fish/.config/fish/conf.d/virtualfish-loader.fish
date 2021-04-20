7105640aa125fac005ba1bcd97a96df9990233e6
Josh Lindsay <git@j000sh.com>
2021-04-19 18:22:07 -0700

    remove asdf shims and completions, move asdf to .config

--- stow/fish/.config/fish/conf.d/virtualfish-loader.fish
+++ stow/fish/.config/fish/conf.d/virtualfish-loader.fish
@@ -1,10 +1,13 @@
 set -g VIRTUALFISH_VERSION 2.5.1
 set -g VIRTUALFISH_PYTHON_EXEC /usr/local/opt/python@3.9/bin/python3.9
 
-set -g VIRTUALENVWRAPPER_PYTHON (command which python3)
+set -g VIRTUALENVWRAPPER_PYTHON (which python3)
 set -g WORKON_HOME ~/.local/envs
 set -g VIRTUALFISH_HOME ~/.local/envs
 set -g PROJECT_HOME ~/Workspaces/Python
 
 source /usr/local/lib/python3.9/site-packages/virtualfish/virtual.fish
+source /usr/local/lib/python3.9/site-packages/virtualfish/compat_aliases.fish
+source /usr/local/lib/python3.9/site-packages/virtualfish/projects.fish
+source /usr/local/lib/python3.9/site-packages/virtualfish/update_python.fish
 emit virtualfish_did_setup_plugins
--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -1,6 +1,5 @@
-source ~/.asdf/asdf.fish
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+source ~/.config/asdf/asdf.fish
+source ~/.iterm2_shell_integration.fish
 
 source (brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 
